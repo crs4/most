a0d2b5b2a5d78e8e3148c37b8adaac306f6d9b65
Francesco Cabras <paneb.devel@gmail.com>
2014-07-25 16:31:44 +0200

    added license header to source

--- examples/most/static/js/demo/task_group.js
+++ examples/most/static/js/demo/task_group.js
@@ -1,3 +1,12 @@
+/*!
+ * Project MOST - Moving Outcomes to Standard Telemedicine Practice
+ * http://most.crs4.it/
+ *
+ * Copyright 2014, CRS4 srl. (http://www.crs4.it/)
+ * Dual licensed under the MIT or GPL Version 2 licenses.
+ * See license-GPLv2.txt or license-MIT.txt
+ */
+
 $(function() {
     $('#task_group_func_1').click(function(){
         console.log('in task_group_func_1 click event');
